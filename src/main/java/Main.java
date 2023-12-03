2359cb8e80b8f1158e499bf03a21ba5f4d10c37c
LoomsyTiger <juliusheenk@gmail.com>
2023-12-03 14:17:43 +0100

    Added player entry and selection for breaking player

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Players players = new Players();
+        SnookerGame snookerGame = new SnookerGame();
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter name for player 1: ");
+        System.out.println("Enter name for player 1:");
         String player1 = input.nextLine();
-        System.out.println("Enter name for player 2: ");
+        System.out.println("Enter name for player 2:");
         String player2 = input.nextLine();
         players.setPlayer1(player1);
         players.setPlayer2(player2);
 
-        System.out.println("These players have been entered: ");
+        System.out.println("These players have been entered:");
         System.out.println(players.getPlayerNames());
+
+        snookerGame.startGame();
     }
 }
---
+++ src/main/java/SnookerGame.java
@@ -0,0 +1,46 @@
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class SnookerGame {
+    int pointsRemaining = 147;
+    int player1score = 0;
+    int player2score = 0;
+    int breakingPlayer;
+    Map<String, Integer> ballValues = new TreeMap<String, Integer>();
+
+    public SnookerGame(){
+        ballValues.put("Red", 1);
+        ballValues.put("Yellow", 2);
+        ballValues.put("Green", 3);
+        ballValues.put("Brown", 4);
+        ballValues.put("Blue", 5);
+        ballValues.put("Pink", 6);
+        ballValues.put("Black", 7);
+    }
+
+    public void startGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Who will break?");
+        System.out.println("1. Player 1");
+        System.out.println("2. Player 2");
+
+        int choice;
+        while (true) {
+            System.out.println("Enter your choice (1 or 2): ");
+            if(scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                if (choice == 1 || choice == 2) {
+                    breakingPlayer = choice;
+                    break;
+                } else {
+                    System.out.println("Invalid choice. Please enter 1 or 2.");
+                }
+            } else {
+                    System.out.println("Invalid input. Please enter a number.");
+                    scanner.next();
+                }
+            }
+        }
+    }
